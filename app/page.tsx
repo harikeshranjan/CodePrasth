d1646e31ebe5442997c41bca38715334952d0bc5
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2025-04-02 20:50:15 +0530

    Minor issue fixed

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,6 @@ export default function Home() {
         </div>
         <LanguageBoxes
           languages={languages}
-          title="reference"
         />
       </section>
     </main>
--- app/reference/page.tsx
+++ app/reference/page.tsx
@@ -34,7 +34,6 @@ export default function ReferencePage() {
         </div>
         <LanguageBoxes
           languages={languages}
-          title="reference"
         />
       </section>
     </main>

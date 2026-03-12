68f51855d49e15c5cca25b41154cf620924dc7bb
Harikesh Ranjan Sinha <harikeshranjansinha14@gmail.com>
2026-03-12 22:49:40 +0530

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,65 @@
-"use client";
-
-import LanguageBoxes from "@/components/language-boxes";
-import { Button } from "@/components/ui/button";
-import { languages } from "@/lib/datalists";
-import { ChevronDownIcon } from "lucide-react";
-import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Home() {
-  const router = useRouter();
-
   return (
-    <main className="w-full">
-      <section className="md:h-screen flex flex-col justify-center items-center space-y-12 py-32 md:ml-16 md:mr-16 border-x border-foreground/20 border-dashed">
-        <h1 className="w-full text-4xl md:text-5xl font-bold text-primary/95 px-16 py-5 mt-10 text-center">Code<span className="text-5xl md:text-7xl">Prasth</span> - The directory for developers</h1>
-        <p className="text-lg md:text-xl text-primary/50 text-center px-16 max-w-4xl">Never struggle with setup again! We provides ready-to-use <span className="text-primary">code snippets</span> for the trickiest coding tasks—just <span className="text-primary">copy, paste, and code faster</span>.</p>
-        <div className="w-full flex flex-col md:flex-row justify-center items-center">
-          <div className="space-x-5 md:space-y-0 md:space-x-4 p-10">
-            <Button
-              variant={"default"}
-              className="h-12 w-28 text-lg tracking-wide cursor-pointer"
-              onClick={() => router.push("/reference")}
-            >
-              Reference
-            </Button>
-            <Button
-              variant={"outline"}
-              className="h-12 w-28 text-lg tracking-wide cursor-pointer"
-              onClick={() => router.push("/learn")}
-            >
-              Learn
-            </Button>
-            <p className="text-primary/50 text-center mt-5 text-sm tracking-wide">
-              More to come soon :&#41;
-            </p>
-          </div>
-        </div>
-
-        <div className="flex flex-col justify-center items-center gap-4">
-          <p className="text-muted-foreground text-center text-sm animate-pulse">
-            Scroll down to see more</p>
-          <ChevronDownIcon size={32} className="text-foreground/50 animate-bounce cursor-pointer hover:text-foreground/80" />
-        </div>
-      </section>
-
-      <section className="border-t border-foreground/20 border-dashed border-x md:ml-16 md:mr-16 px-10">
-        <div className="py-10 ">
-          <h2 className="text-4xl text-center font-bold">Quick Links</h2>
-        </div>
-        <LanguageBoxes
-          languages={languages}
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
         />
-      </section>
-    </main>
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started, edit the page.tsx file.
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Templates
+            </a>{" "}
+            or the{" "}
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Learning
+            </a>{" "}
+            center.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
+            Deploy Now
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+        </div>
+      </main>
+    </div>
   );
 }

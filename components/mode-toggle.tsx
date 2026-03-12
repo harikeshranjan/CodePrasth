b0e834cdb1d05f5f84e36dcb72724f5f176120ad
Harikesh Ranjan Sinha <harikeshranjansinha14@gmail.com>
2026-03-12 22:56:47 +0530

    [feat(theme)] add theme provider and toggle functionality

---
+++ components/mode-toggle.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
+import { Sun } from "lucide-react";
+import { Moon } from "lucide-react";
+
+export default function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+  return (
+    <Button
+      variant="outline"
+      size={"icon"}
+      onClick={() => {
+        setTheme(theme === "dark" ? "light" : "dark");
+      }}
+    >
+      {theme === "dark" ? <Sun /> : <Moon />}
+    </Button>
+  );
+}

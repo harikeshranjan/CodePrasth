eda28c430bea860ce6035a1a01f3a344f379bf02
Harikesh Ranjan Sinha <harikeshranjansinha14@gmail.com>
2026-03-15 13:28:26 +0530

    [feat(layout/footer)] Add footer component and integrate into root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -40,6 +41,7 @@ export default function RootLayout({
         >
           <Header />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
